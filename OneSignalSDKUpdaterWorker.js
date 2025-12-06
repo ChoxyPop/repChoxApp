4f564de228991fb4e5acd6f826d755ce0f0f035e
Choxy <choxypop@gmail.com>
2025-12-06 18:13:38 +0000

    Update OneSignalSDKWorker.js

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,1 @@
-// OneSignalSDKUpdaterWorker.js
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js");
